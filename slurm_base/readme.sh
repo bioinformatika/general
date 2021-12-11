27adcbd0ba5448dd17e829b59a6a2c384ecbc5af
dovelleiro <dovelleiro@gmail.com>
2021-12-11 19:23:22 +0100

    added files in slurm_base

--- slurm_base/readme.sh
+++ slurm_base/readme.sh
@@ -1,15 +1,14 @@
-
 # We build the docker image
 docker build -t slurm_base .
 
 # Create a container
 DOCKER_ID=$(docker run -d -v /mnt/:/mnt -m 28g --cpus 10 -h controlmachine -i slurm_base)
 
-
-
+# And test that Java, Slurm and Conda ar up and running
 docker exec -i $DOCKER_ID sinfo
 docker exec -i $DOCKER_ID java -version
 docker exec -i $DOCKER_ID conda env list
 
+# We can stop the running container
 docker container ls
 docker stop $DOCKER_ID
